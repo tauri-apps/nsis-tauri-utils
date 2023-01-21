000d6326333f862741f1514de34542316445951e
Amr Bashir <amr.bashir2015@gmail.com>
2023-01-21 03:44:36 +0200

    ci: setup CI/CD and covector (#2)
    * ci: setup CI/CD and covector
    
    * remove system deps
    
    * update versions
    
    * update windows-sys
    
    * use `GITHUB_TOKEN`
    
    * fix covector getPublishedVersion
    
    * log data
    
    * bump all to minor
    
    * fix change file

---
+++ .changes/initial-releas.md
@@ -0,0 +1,8 @@
+---
+"nsis_tauri_utils": "minor"
+"nsis_download": "minor"
+"nsis_process": "minor"
+"nsis_semvercompare": "minor"
+---
+
+Initial Release.
---
+++ .scripts/package-latest-version.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+// Copyright 2019-2022 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+/*
+This script is solely intended to be run as part of the `covector publish` step to
+check the latest version of a crate, considering the current minor version.
+*/
+
+const https = require('https')
+
+const packageName = process.argv[2]
+const packageVersion = process.argv[3]
+const target = packageVersion.substring(0, packageVersion.lastIndexOf('.'))
+
+
+const options = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": process.env["GITHUB_TOKEN"] ? `Bearer <${process.env["GITHUB_TOKEN"]}>` : null,
+        'User-Agent': 'tauri (https://github.com/tauri-apps/nsis-tauri-utils)'
+    }
+}
+
+https.get("https://api.github.com/repos/tauri-apps/nsis-tauri-utils/releases", options, (response) => {
+    let chunks = []
+    response.on('data', function (chunk) {
+        chunks.push(chunk)
+    })
+
+    response.on('end', function () {
+        const data = JSON.parse(chunks.join(''))
+        const versions = data.filter(t => t.tag_name.startsWith(`${packageName}-v${target}`))
+        console.log(versions.length ? versions[0].tag_name : '0.0.0')
+
+    })
+})
